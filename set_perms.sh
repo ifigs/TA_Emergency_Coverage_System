a13e6738e0d17ed783fb2d8051126f2cf3868067
starryclouds <32180295+starryclouds@users.noreply.github.com>
2018-01-11 17:11:20 -0800

    Delete set_perms.sh

--- set_perms.sh
+++
@@ -1,9 +0,0 @@
-find . -name "*.php" -exec chmod 755 {} \;
-find . -name "*.html" -exec chmod 644 {} \;
-find . -name "*.js" -exec chmod 644 {} \;
-find . -name "*.css" -exec chmod 644 {} \;
-chmod 777 data
-chmod 711 .
-chmod 711 images
-chmod 644 images/ui-icons_444444_256x240.png
-chmod 644 images/ui-icons_555555_256x240.png
